c060578e40da9521dbd9a3b8714a7ef06c21ffdc
DiegoGuerra00 <di.guerra00@gmail.com>
2022-11-22 19:50:03 -0300

    new keybindings for i3 and rounded corners for picom

--- post_install.sh
+++ post_install.sh
@@ -5,18 +5,9 @@ read -r confirmChoice
 
 case "$confirmChoice" in
     y|Y|yes|YES|Yes) 
-       read -r "Would you want to upgrade the system before?" upgradeChoice
-       case "$upgradeChoice" in
-        y|Y|yes|YES|Yes) 
-            sudo xbps-install -Syu
-        ;;
-        n|N|No|no|NO)
-            echo "Skipping system upgrade..."
-        ;;
-       esac
        mkdir Downloads Pictures Files Games
        echo "Installing i3, polybar and other rice applications..."
-       sudo xbps-install -Ry i3-gaps polybar rofi ranger picom dunst allacrity neovim git curl wget 
+       sudo xbps-install -SRy i3-gaps polybar rofi ranger picom dunst alacritty neovim curl wget zsh stow
        echo "Setting up lightdm"
        sudo xbps-install -Ry lightdm lightdm-gtk3-greeter ConsoleKit2 dbus
        ln -s /etc/sv/bus /var/service/dbus
